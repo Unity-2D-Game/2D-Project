df6a527e0be40f6ab37885a512d937ecf8a7598d
jongmmk <120369117+jongmmk@users.noreply.github.com>
2026-05-21 21:02:13 +0900

    gun-object 작업

---
+++ Assets/script/GunScript.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    [Header("Bullet Settings")]
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 20f;
+
+    [Header("Fire Settings")]
+    public float fireRate = 0.2f;
+    private float nextFireTime;
+
+    void Update()
+    {
+        Aim();
+
+        if (Input.GetMouseButton(0) && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + fireRate;
+            Shoot();
+        }
+    }
+
+    void Aim()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+
+        Vector2 direction = mousePos - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+
+        Bullet b = bullet.GetComponent<Bullet>();
+        b.SetVelocity(firePoint.right, bulletSpeed);
+    }
+}
